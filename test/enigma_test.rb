2a8919bfc41669bfc0048cb122cef0269c3bc684
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-02 10:42:58 -0600

    Adds first test for Enigma class.

---
+++ test/enigma_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require 'date'
+require 'pry'
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+  end
+end
