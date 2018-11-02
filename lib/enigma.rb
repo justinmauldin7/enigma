97db0dd67daad9dfa9374356cf200d9b576cf063
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-02 12:08:42 -0600

    Adds date_conversion method.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,19 @@
 class Enigma
-  def encrypt(message, key, date)
+  def encrypt(message, key, date = Date.today)
   end
 
   def decrypt(ciphertext, key, date)
-  end 
+  end
+
+  def date_conversion(date)
+    date_string = ""
+    date_array = date.to_s.split('-')
+    two_digit_array = date_array.map do |index|
+      index[-2..-1]
+    end
+  two_digit_array.rotate(1).each do |index|
+    date_string << index
+    end
+    date_string
+  end
 end
