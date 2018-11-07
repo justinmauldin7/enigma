e1f5b88c6856490a37380b6a4c9f8e8b28884e2c
Justin Mauldin <justin.mauldin7@gmail.com>
2018-11-07 10:52:29 -0700

    Adds changes to enigma tests so they are not date dependent.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -26,7 +26,7 @@ class EnigmaTest < Minitest::Test
 
   def test_it_can_convert_todays_date_to_a_formatted_string
     enigma = Enigma.new
-    assert_equal "061118", enigma.date_conversion(Date.today)
+    assert_equal 6, enigma.date_conversion(Date.today).length
   end
 
   def test_it_can_generate_a_random_key
@@ -48,20 +48,16 @@ class EnigmaTest < Minitest::Test
 
   def test_it_encrypts_a_message_with_todays_date
     enigma = Enigma.new
-    encrypted = enigma.encrypt("hello world", "02715")
-    expected = {
-      encryption: "snddziogbuw",
-      key: "02715",
-      date: "061118"
-    }
-    assert_equal expected, encrypted
+    assert_equal 11 , enigma.encrypt("hello world", "02715")[:encryption].length
+    assert_equal 5 , enigma.encrypt("hello world", "02715")[:key].length
+    assert_equal 6 , enigma.encrypt("hello world", "02715")[:date].length
   end
 
   def test_it_encrypts_a_message_with_todays_date_and_random_key
     enigma = Enigma.new
     assert_equal 11 , enigma.encrypt("hello world")[:encryption].length
     assert_equal 5 , enigma.encrypt("hello world")[:key].length
-    assert_equal "061118", enigma.encrypt("hello world")[:date]
+    assert_equal 6 , enigma.encrypt("hello world")[:date].length
   end
 
   def test_it_decrypts_a_message
@@ -76,11 +72,8 @@ class EnigmaTest < Minitest::Test
 
   def test_it_decrypts_a_message_with_todays_date
     enigma = Enigma.new
-    expected = {
-      decryption: "hello world",
-      key: "02715",
-      date: "061118"
-    }
-    assert_equal expected, enigma.decrypt("snddziogbuw", "02715")
+    assert_equal "hello world", enigma.decrypt("snddziogbuw", "02715")[:decryption]
+    assert_equal "02715", enigma.decrypt("snddziogbuw", "02715")[:key]
+    assert_equal 6, enigma.decrypt("snddziogbuw", "02715")[:date].length
   end
 end
